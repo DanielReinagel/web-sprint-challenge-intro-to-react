bf7535b32204d281ea092f804cfbc200cd7ddf13
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-10 09:27:47 -0700

    MVP reached

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import styled from 'styled-components';
+
+const BlueP = styled.p`
+    color: yellow;
+    text-shadow: 1px 1px 5px black;
+    font-size: 2rem;
+`;
+
+function Character(props){
+    console.log(props.character);
+    return (
+        <BlueP>{props.character.name}</BlueP>
+    );
+}
+
+export default Character;
